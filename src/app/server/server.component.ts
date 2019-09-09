4bc0136590e000a1fcea811c90a47bf0ef9af8a0
biswajitkar01 <44807828+biswajitkar01@users.noreply.github.com>
2019-09-09 19:38:32 +0530

    on 09th

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,31 +2,48 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent {
   serverStatus: string = "Processing";
   serverName = "";
   serverButton = false;
+  serverCreated = false;
+  serverss = [];
 
   constructor() {
     setTimeout(() => {
       this.serverButton = true;
     }, 2000);
-    setTimeout(() => {
-      this.serverStatus = "No server Added!!";
-    }, 2000);
+    // setTimeout(() => {
+    //   this.serverStatus = "No server Added!!";
+    // }, 2000);
+
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
     // if (this.serverName === "") {
     //   this.serverButton = false;
   }
 
   onAddServer() {
-    this.serverStatus = "server added-->" + this.serverName;
-    this.serverName = "";
+    this.serverCreated = true;
+    this.serverss.push(this.serverName);
+    //this.serverStatus = "server added-->" + this.serverName;
+    // this.serverName = "";
     // this.serverName = this.serverName;
   }
   // onEnterServer() {
   //   return (this.serverButton = true);
   //   // this.serverName = event.value;
   // }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
+  }
 }
